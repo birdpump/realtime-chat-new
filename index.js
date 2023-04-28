cfbdab23b8d21055e3d7a6068acfbf940750d37e
marc <birdpump@gmai.com>
2023-04-28 09:04:39 -0700

    main

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 //data.json 
-const datas = require("./data");
 const fs = require("fs");
+
+let datas = [];
+
+function loadData() {
+  try {
+    const fileContents = fs.readFileSync("./data.json");
+    datas = JSON.parse(fileContents);
+  } catch (err) {
+    datas = [];
+  }
+}
+
+loadData();
+
+fs.watch("./data.json", () => {
+  loadData();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/test2.html');
@@ -28,25 +44,36 @@ io.on('connection', (socket) => {
     socket.on('rm_post', (post) => {
         console.log(`Post: ${post.index} deleted`);
         datas.splice(5, 1);
-       
     });
 
     socket.on('event', (usr) => {
         
         // console.log(`User: ${usr.user}`)
-        // console.log(`Msg: ${usr.msg}`)
-        
-        let msgid = datas.length + 1;
-        //write to data.json
-        let msgdata = {
-            msg: usr.msg,
-            user: usr.user,
-            id: msgid,
-        };
-        datas.push(msgdata);
-        fs.writeFile("data.json", JSON.stringify(datas), err => { });
-        
-        io.emit('chat message', { user: usr.user, msg: usr.msg, id: datas.length });
+        console.log(`Msg: ${usr.msg}`)
+
+    if(usr.msg.includes("!cmd")){
+        if(usr.msg === "!cmd rm"){
+            console.log("removing data");
+            fs.writeFile("data.json", "[]", function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file was emptied!");
+            });
+        }
+    }else{
+            let msgid = datas.length + 1;
+            //write to data.json
+            let msgdata = {
+                msg: usr.msg,
+                user: usr.user,
+                id: msgid,
+            };
+            datas.push(msgdata);
+            fs.writeFile("data.json", JSON.stringify(datas), err => { });
+
+            io.emit('chat message', { user: usr.user, msg: usr.msg, id: datas.length });
+    }
     });
 });
 
